c71f80ab1ffe4603b8c44e2e32820da44439368c
Yousef Omar <yousefo1995@gmail.com>
2023-07-02 14:04:10 +0300

    handle signout , login and add a condition for register without adding a file

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,23 @@
-import React from "react";
-import { profileImage } from "../links";
+import React, { useContext } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase.js";
+import { AuthContext } from "../context/AuthContext";
+// import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  // const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
+  const handleLogout = () => {
+    signOut(auth);
+    // navigate("/login"); // use protected routes instead of this
+  };
   return (
     <div className="navBar">
       <span className="logo">Chat app</span>
       <div className="user">
-        <img src={profileImage} alt="" />
-        <span>name</span>
-        <button>Logout </button>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={handleLogout}>Logout </button>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,46 +4,59 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { Link, useNavigate } from "react-router-dom";
+
 const Register = () => {
   const [error, setError] = useState(false);
-
+  const [emptyFieldMessage, setEmptyFieldMessage] = useState("");
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
-    try {
-      const res = await createUserWithEmailAndPassword(auth, email, password);
+    if (
+      file !== undefined &&
+      email !== "" &&
+      password !== "" &&
+      displayName !== ""
+    ) {
+      try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
 
-      const storageRef = ref(storage, displayName);
+        const storageRef = ref(storage, displayName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
 
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        (error) => {
-          setError(true);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            await updateProfile(res.user, {
-              displayName: displayName,
-              photoURL: downloadURL,
-            });
-            await setDoc(doc(db, "users", res.user.uid), {
-              uid: res.user.uid,
-              displayName,
-              email,
-              photoURL: downloadURL,
-            });
-            await setDoc(doc(db, "userChats", res.user.uid), {});
-            // navigate to home page
-          });
-        }
-      );
-    } catch (error) {
-      console.log(error);
-      setError(true);
+        uploadTask.on(
+          (error) => {
+            setError(true);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then(
+              async (downloadURL) => {
+                await updateProfile(res.user, {
+                  displayName: displayName,
+                  photoURL: downloadURL ? downloadURL : null,
+                });
+                await setDoc(doc(db, "users", res.user.uid), {
+                  uid: res.user.uid,
+                  displayName,
+                  email,
+                  photoURL: downloadURL ? downloadURL : null,
+                });
+                await setDoc(doc(db, "userChats", res.user.uid), {});
+                navigate("/");
+              }
+            );
+          }
+        );
+      } catch (error) {
+        console.log(error);
+        setError(true);
+      }
+    } else {
+      setEmptyFieldMessage(true);
     }
   };
   return (
@@ -65,8 +78,15 @@ const Register = () => {
             {error && (
               <span className="errorMessage">!! Something went wrong</span>
             )}
+            {emptyFieldMessage && (
+              <span className="errorMessage">
+                Please fill out all fields before submitting.
+              </span>
+            )}
           </form>
-          <p>You have an account? Login</p>
+          <p>
+            You have an account? <Link to="/login">Login</Link>
+          </p>
         </div>
       </div>
     </div>
