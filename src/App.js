88d3f6c4ad333a96d6b6cca20134fe8869aa4e25
Yousef Omar <yousefo1995@gmail.com>
2023-07-02 14:02:44 +0300

    Create Auth context and handle routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,39 @@ import "./App.css";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import "./style.scss";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
