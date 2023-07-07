1f45ffc08606dd2593bae3c2894e25acafba1d32
Yousef Omar <yousefo1995@gmail.com>
2023-07-07 17:25:42 +0300

    handle a default profile image and handle search keypress on phone

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,6 +6,7 @@ import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import { Link, useNavigate } from "react-router-dom";
 
+const defaultImage = "https://i.stack.imgur.com/l60Hf.png";
 const Register = () => {
   const [error, setError] = useState(false);
   const [emptyFieldMessage, setEmptyFieldMessage] = useState("");
@@ -13,53 +14,69 @@ const Register = () => {
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsLoading(true);
-    const displayName = e.target[0].value;
-    const email = e.target[1].value;
-    const password = e.target[2].value;
-    const file = e.target[3].files[0];
-    if (
-      file !== undefined &&
-      email !== "" &&
-      password !== "" &&
-      displayName !== ""
-    ) {
-      try {
-        const res = await createUserWithEmailAndPassword(auth, email, password);
+    if (isLoading === false) {
+      setIsLoading(true);
+      const displayName = e.target[0].value;
+      const email = e.target[1].value;
+      const password = e.target[2].value;
+      const file = e.target[3].files[0];
+      if (email !== "" && password !== "" && displayName !== "") {
+        try {
+          const res = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+          );
 
-        const storageRef = ref(storage, displayName);
-        const uploadTask = uploadBytesResumable(storageRef, file);
+          if (file) {
+            const storageRef = ref(storage, displayName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
 
-        uploadTask.on(
-          (error) => {
-            setError(true);
-          },
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then(
-              async (downloadURL) => {
-                await updateProfile(res.user, {
-                  displayName: displayName,
-                  photoURL: downloadURL ? downloadURL : null,
-                });
-                await setDoc(doc(db, "users", res.user.uid), {
-                  uid: res.user.uid,
-                  displayName,
-                  email,
-                  photoURL: downloadURL ? downloadURL : null,
-                });
-                await setDoc(doc(db, "userChats", res.user.uid), {});
-                navigate("/");
+            uploadTask.on(
+              (error) => {
+                setError(true);
+              },
+              () => {
+                getDownloadURL(uploadTask.snapshot.ref).then(
+                  async (downloadURL) => {
+                    await updateProfile(res.user, {
+                      displayName: displayName,
+                      photoURL: downloadURL,
+                    });
+                    await setDoc(doc(db, "users", res.user.uid), {
+                      uid: res.user.uid,
+                      displayName,
+                      email,
+                      photoURL: downloadURL ? downloadURL : null,
+                    });
+                    await setDoc(doc(db, "userChats", res.user.uid), {});
+                    navigate("/");
+                  }
+                );
               }
             );
+          } else {
+            await updateProfile(res.user, {
+              displayName: displayName,
+              photoURL: defaultImage,
+            });
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: defaultImage,
+            });
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
           }
-        );
-      } catch (error) {
-        console.log(error);
-        setIsLoading(false);
-        setError(true);
+        } catch (error) {
+          console.log(error);
+          setIsLoading(false);
+          setError(true);
+        }
+      } else {
+        setEmptyFieldMessage(true);
       }
-    } else {
-      setEmptyFieldMessage(true);
     }
   };
   return (
